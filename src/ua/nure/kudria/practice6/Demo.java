c0ea9e64b4ec8fc58ab6e0bb72b744657bc56b2f
akudria <ak98nneon@gmail.com>
2019-02-04 14:22:39 +0200

    Change comparator into lambda

--- src/ua/nure/kudria/practice6/Demo.java
+++ src/ua/nure/kudria/practice6/Demo.java
@@ -13,25 +13,25 @@ public class Demo {
         Part1.main(args);
         System.setIn(STD_IN);
 
-        System.out.println("~~~~~~~~~~~~Part2");
-        Part2.main(args);
-
-        System.out.println("~~~~~~~~~~~~Part3");
-        Part3.main(args);
-
-        System.out.println("~~~~~~~~~~~~Part4");
-        Part4.main(args);
-
-        System.out.println("~~~~~~~~~~~~Part5");
-        Part5.main(args);
-
-        System.out.println("~~~~~~~~~~~~Part6");
-        Part6.main(new String[]{"--input", "part6.txt", "--task", "length"});
-        Part6.main(new String[]{"--input", "part6.txt", "--task", "frequency"});
-        Part6.main(new String[]{"--input", "part6.txt", "--task", "duplicates"});
-
-        System.out.println("~~~~~~~~~~~~Part7");
-        Part7.main(args);
+//        System.out.println("~~~~~~~~~~~~Part2");
+//        Part2.main(args);
+//
+//        System.out.println("~~~~~~~~~~~~Part3");
+//        Part3.main(args);
+//
+//        System.out.println("~~~~~~~~~~~~Part4");
+//        Part4.main(args);
+//
+//        System.out.println("~~~~~~~~~~~~Part5");
+//        Part5.main(args);
+//
+//        System.out.println("~~~~~~~~~~~~Part6");
+//        Part6.main(new String[]{"--input", "part6.txt", "--task", "length"});
+//        Part6.main(new String[]{"--input", "part6.txt", "--task", "frequency"});
+//        Part6.main(new String[]{"--input", "part6.txt", "--task", "duplicates"});
+//
+//        System.out.println("~~~~~~~~~~~~Part7");
+//        Part7.main(args);
     }
 
 }
