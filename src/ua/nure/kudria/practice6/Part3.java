183bbdcccbb0731bc8d5507f2677e3ddc23a82b6
akudria <ak98nneon@gmail.com>
2019-02-04 14:57:05 +0200

    Re-order 3-th task

--- src/ua/nure/kudria/practice6/Part3.java
+++ src/ua/nure/kudria/practice6/Part3.java
@@ -1,9 +1,9 @@
 package ua.nure.kudria.practice6;
 
 public class Part3 {
-	
-	public static void main(String[] args) {
-		
-	}
 
+    public static void main(String[] args) {
+        int sizeParking = 5;
+        Parking parking = new Parking(sizeParking);
+    }
 }
