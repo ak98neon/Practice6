a26588912c52f5e1212ababf2deb2290969e3625
akudria <ak98nneon@gmail.com>
2019-02-05 13:59:50 +0200

    Create part3, part4

--- src/ua/nure/kudria/practice6/part3/Parking.java
+++ src/ua/nure/kudria/practice6/part3/Parking.java
@@ -1,41 +1,50 @@
 package ua.nure.kudria.practice6.part3;
 
+import java.util.Objects;
+
 public class Parking {
-//    //Maybe ArrayList?
-//    private Object[] list;
-//
-//    public Parking(int size) {
-//        this.list = new Object[size];
-//    }
-//
-//    public void arrive(int pos, Object car) {
-//        for (int i = 0; i < list.length; i++) {
-//            if (list[i] == null) {
-//                list[i] = car;
-//                return;
-//            }
-//
-//        }
-//        System.out.println("Свободных мест нет");
-//    }
-//
-//    public void depart(Object car) {
-//        try {
-//            if ((list[car] != null)) {
-//                list[car] = null;
-//            } else
-//                System.out.println("Место свободно!");
-//        } catch (IndexOutOfBoundsException e) {
-//            System.out.println("Парковочное место не существует");
-//        }
-//    }
-//
-//    public void print() {
-//        for (final String s : list) {
-//            if (s == null) {
-//                System.out.println("Место свободно");
-//            } else
-//                System.out.println("Место занято");
-//        }
-//    }
+    private Object[] list;
+
+    public Parking(int size) {
+        this.list = new Object[size];
+    }
+
+    public void arrive(int pos, Object car) {
+        if (pos < 0 || pos > list.length - 1) {
+            return;
+        }
+
+        if (list[pos] == null) {
+            list[pos] = car;
+            return;
+        } else {
+            for (int i = pos + 1; i < list.length; i++) {
+                if (list[i] == null) {
+                    list[i] = car;
+                    return;
+                }
+            }
+        }
+        System.out.println("Свободных мест нет");
+    }
+
+    public void depart(Object car) {
+        for (int i = 0; i < Objects.requireNonNull(list).length; i++) {
+            if (list[i] == car) {
+                list[i] = null;
+                System.out.println("Место свободно!");
+                return;
+            }
+        }
+        System.out.println("Car not exist!");
+    }
+
+    public void print() {
+        for (final Object s : list) {
+            if (s == null) {
+                System.out.println("Место свободно");
+            } else
+                System.out.println("Место занято");
+        }
+    }
 }
--- src/ua/nure/kudria/practice6/part3/Part3.java
+++ src/ua/nure/kudria/practice6/part3/Part3.java
@@ -4,6 +4,16 @@ public class Part3 {
 
     public static void main(String[] args) {
         int sizeParking = 5;
-//        Parking parking = new Parking(sizeParking);
+        Object testCar = new Object();
+        Parking parking = new Parking(sizeParking);
+        parking.arrive(0, testCar);
+        parking.depart(testCar);
+        parking.arrive(0, testCar);
+        parking.arrive(1, new Object());
+        parking.arrive(2, new Object());
+        parking.arrive(3, new Object());
+        parking.arrive(4, new Object());
+        parking.arrive(5, new Object());
+        parking.print();
     }
 }
